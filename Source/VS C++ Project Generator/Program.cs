9a54241d274df6802d81cd7394ad0b578f1f1f40
Alex Sindledecker <alexsindledecker1@gmail.com>
2022-05-18 22:17:11 -0700

    Project setup

---
+++ Source/VS C++ Project Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS_C___Project_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
