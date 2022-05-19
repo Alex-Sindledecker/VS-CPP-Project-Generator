7d8adec4fe5a76f2be0d91a06a81210e760c5a92
Alex Sindledecker <alexsindledecker1@gmail.com>
2022-05-19 12:37:19 -0700

    Replaced IOStream with just the normal console

--- Source/VS C++ Project Generator/Prompts/IPrompt.cs
+++ Source/VS C++ Project Generator/Prompts/IPrompt.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using VS_CPP_Project_Generator.Logging;
 using VS_CPP_Project_Generator.Models;
 
 namespace VS_CPP_Project_Generator.Prompts
 {
     public interface IPrompt
     {
-        public void Show(ILogger logger);
-        public bool Validate();
+        public void Show();
+        public void ShowFailedValidationMessage();
+        public bool Validate(string userInput);
         public void Populate(ProjectModel model);
     }
 }
