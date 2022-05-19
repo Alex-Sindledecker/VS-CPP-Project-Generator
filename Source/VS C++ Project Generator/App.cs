6b0cab75e0f0af68f134dfab7b25b54cdbc63249
Alex Sindledecker <alexsindledecker1@gmail.com>
2022-05-19 15:00:04 -0700

    Adjusted directories and added DependencyURLPrompt

--- Source/VS C++ Project Generator/App.cs
+++ Source/VS C++ Project Generator/App.cs
@@ -9,22 +9,38 @@ namespace VS_CPP_Project_Generator
     {
         static void Main(string[] args)
         {
-            //ProjectModelGenerator projectModelGenerator = new ProjectModelGenerator();
+            DependencyURLPrompt dependencyURLPrompt = new DependencyURLPrompt();
+            bool test1 = dependencyURLPrompt.Validate("https://github.com/Alex-Sindledecker/VS-CPP-Project-Generator/tree/development");
+            bool test2 = dependencyURLPrompt.Validate("https://github.com/canton7/Stylet");
+            bool test3 = dependencyURLPrompt.Validate("https://github.com/Alex-Sindledecker/VS-CPP-Project-Generator");
+            bool test4 = dependencyURLPrompt.Validate("https://www.w3schools.com/cs/cs_user_input.php");
+            bool test5 = dependencyURLPrompt.Validate("https://github.com/SFML/SFML/releases/download/2.5.1/SFML-2.5.1-windows-vc15-64-bit.zip");
 
-            //projectModelGenerator.AddPrompt(new ProjectNamePrompt());
-            //projectModelGenerator.AddPrompt(new DiskLocationPrompt());
-            //projectModelGenerator.AddPrompt(new ProjectTypePrompt());
-            
-            //ProjectModel model = projectModelGenerator.RunPrompts();
+            //ProjectModel model = GetProjectModel();
+        }
 
-            //Console.WriteLine(model.DiskLocation);
+        static ProjectModel GetProjectModel()
+        {
+            ProjectModelGenerator projectModelGenerator = new ProjectModelGenerator();
+            DependencyModelGenerator dependencyModelGenerator = new DependencyModelGenerator();
+
+            projectModelGenerator.AddPrompt(new ProjectNamePrompt());
+            projectModelGenerator.AddPrompt(new DiskLocationPrompt());
+            projectModelGenerator.AddPrompt(new ProjectTypePrompt());
+
+            ProjectModel model = projectModelGenerator.RunPrompts();
 
             ConditionalPrompt conditionalPrompt = new ConditionalPrompt("Add another dependency?");
-            do
+            PromptCommon.RunPrompt(conditionalPrompt);
+            while (conditionalPrompt.Result == true)
             {
+                DependencyModel dependency = dependencyModelGenerator.RunPrompts();
+                model.Dependencies.Add(dependency);
+
                 PromptCommon.RunPrompt(conditionalPrompt);
             }
-            while (conditionalPrompt.Result == true);
+
+            return model;
         }
     }
 }
---
+++ Source/VS C++ Project Generator/Prompts/DependencyPrompts/DependencyURLPrompt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using VS_CPP_Project_Generator.Models;
+
+namespace VS_CPP_Project_Generator.Prompts
+{
+    class DependencyURLPrompt : IDependencyPrompt
+    {
+        private string _url;
+
+        public void Populate(DependencyModel model)
+        {
+            model.Url = _url;
+        }
+
+        public void Show()
+        {
+            Console.Write("Dependency url (github repo or zip file): ");
+        }
+
+        public void ShowFailedValidationMessage()
+        {
+            Console.WriteLine("Invalid url!");
+        }
+
+        public bool Validate(string userInput)
+        {
+            if (Uri.IsWellFormedUriString(userInput, UriKind.Absolute))
+            {
+                Regex zipExtensionRegex = new Regex(@"^.*\.(zip)$");
+                Regex githubURLRegex = new Regex(@"https:\/\/github.com\/[a-zA-Z|\-|0-9]+\/[a-zA-Z|\-|0-9]+$");
+                if (zipExtensionRegex.IsMatch(userInput) == true || githubURLRegex.IsMatch(userInput))
+                {
+                    _url = userInput;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
