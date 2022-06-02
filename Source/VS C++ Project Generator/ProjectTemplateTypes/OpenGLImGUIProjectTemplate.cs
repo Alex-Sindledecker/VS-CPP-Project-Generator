a08dc3594e9d30bc0c5b545ce03de41dc0d9a23a
Alex Sindledecker <alexsindledecker1@gmail.com>
2022-06-02 16:55:44 -0700

    Changed the implementation of project templates. Now very easy to add new templates without modifying existing code

---
+++ Source/VS C++ Project Generator/ProjectTemplateTypes/OpenGLImGUIProjectTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VS_CPP_Project_Generator.Models;
+using VS_CPP_Project_Generator.Models.ModelGenerators;
+using VS_CPP_Project_Generator.ProjectAssembly;
+
+namespace VS_CPP_Project_Generator.ProjectTemplateTypes
+{
+    [ProjectTemplate]
+    public class OpenGLImGUIProjectTemplate
+    {
+        public static string GetName()
+        {
+            return "OpenGL (with imgui)";
+        }
+
+        public static void PopulateProjectModel(ProjectModel model)
+        {
+            model.TemplateSourcePath = $"{PathTools.GetTemplateRootPath()}ImGuiSource/";
+            model.Dependencies.Add(DependencyModelGenerator.GetGLADModel());
+            model.Dependencies.Add(DependencyModelGenerator.GetGLFWModel());
+            model.Dependencies.Add(DependencyModelGenerator.GetGLMModel());
+            model.Dependencies.Add(DependencyModelGenerator.GetImGuiModel());
+        }
+    }
+}
---
+++ Source/VS C++ Project Generator/ProjectTemplateTypes/ProjectTemplateAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VS_CPP_Project_Generator.ProjectTemplateTypes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ProjectTemplateAttribute : Attribute
+    {
+    }
+}
